e3c7507bff50e580c35ca2e71830785a8d221484
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-16 17:44:52 -0600

    Export all container files to reduce clutter in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { About, Footer, Header, Projects, Skills, Testimonial } from './containers/index';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app" >
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
