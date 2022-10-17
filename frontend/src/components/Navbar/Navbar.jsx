f32534ae03d970aa26eec9f7065e5ac6f5a94fbb
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-17 15:45:27 -0600

    Create mobile menu

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from '../../constants';
 import './Navbar.scss'
+import { motion } from 'framer-motion';
+import { HiOutlineMenu, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(true);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo" >
@@ -15,6 +20,23 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+          <HiOutlineMenu onClick={() => setToggle(true)} />
+            {toggle && (
+              <motion.div
+                whileInView={{x: [300, 0]}}
+                transition={{duration: 0.8, ease: 'ease-in-out'}}
+              >
+                <HiX onClick={() => setToggle(false)} />
+                {['home', 'projects', 'skills', 'about', 'contact'].map((item) => (
+                  <li className="app__flex p-text" key={item}>
+                    <a href={`#${item}`}>{item} onClick={() => setToggle(false)}</a>
+                  </li>
+                ))}
+              </motion.div>
+            )}
+      </div>
     </nav>
   )
 }
