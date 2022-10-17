0d85d6f956c236110d04e06eaa8c6defc3617afe
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-17 17:42:34 -0600

    Add ul tags

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { HiOutlineMenu, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
 
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="app__navbar">
@@ -25,15 +25,17 @@ const Navbar = () => {
           <HiOutlineMenu onClick={() => setToggle(true)} />
             {toggle && (
               <motion.div
-                whileInView={{x: [300, 0]}}
+                whileInView={{x: [280, 0]}}
                 transition={{duration: 0.8, ease: 'ease-in-out'}}
               >
                 <HiX onClick={() => setToggle(false)} />
-                {['home', 'projects', 'skills', 'about', 'contact'].map((item) => (
-                  <li className="app__flex p-text" key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                  </li>
-                ))}
+                <ul>
+                  {['home', 'projects', 'skills', 'about', 'contact'].map((item) => (
+                    <li key={item}>
+                      <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                    </li>
+                  ))}
+                </ul>
               </motion.div>
             )}
       </div>
