5569bba4f275b53304a5f951666dd59e545d748e
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-18 13:52:29 -0600

    Background circle

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,7 +1,10 @@
 import logo from '../assets/logo.png';
 import profile from '../assets/profile.gif';
+import circle from '../assets/circle.svg';
+import { images } from '.';
 
 export default {
   logo,
-  profile 
+  profile,
+  circle,
 };
