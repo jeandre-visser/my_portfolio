457f16779669ac12ef07cd542066d3076f52ac81
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-18 13:09:52 -0600

    Create name badge with framer motion

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './Header.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app_header app_flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1]}}
+        transitions={{ duration: 4 }}
+      >
+        <div className="app_header-badge">
+          <div className="badge-cmp app__flex">
+            <div style={{ marginLeft: 25}}>
+              <p className="p-text">Welcome, I'm</p>
+              <h1 className="head-text">Jeandre</h1>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </div>  
   )
 }
 
