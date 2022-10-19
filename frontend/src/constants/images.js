ec6a98a660c8db59640973183959ecc9494b4ac6
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-19 14:40:04 -0600

    Add new images for about me section

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -4,6 +4,10 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import bgIMG from '../assets/bgIMG.png';
 import react from '../assets/react.png';
+import aboutMe01 from '../assets/aboutMe01.png';
+import aboutMe02 from '../assets/aboutMe02.png';
+import aboutMe03 from '../assets/aboutMe03.png';
+import aboutMe04 from '../assets/aboutMe04.gif';
 
 export default {
   logo,
@@ -12,4 +16,8 @@ export default {
   sass,
   react,
   bgIMG,
+  aboutMe01,
+  aboutMe02,
+  aboutMe03,
+  aboutMe04
 };
