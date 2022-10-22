4358831b994694fa54e9845aa9cc082c7b02df4d
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-22 14:56:09 -0600

    Empty object instead of empty quotes

--- frontend/src/components/NavDots.jsx
+++ frontend/src/components/NavDots.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const NavDots = ( { active }) => {
+const NavDots = ({ active }) => {
   return (
     <div className="app__navigation">
       {['home', 'about', 'skills', 'projects', 'contact'].map((item, index) => (
         <a href={`#${item}`} 
           className="app__navigation-dot"
           key={item + index}
-          style={active === item ? {backgroundColor: '#000'} : '' }
+          style={active === item ? {backgroundColor: '#000'} : {}}
         >
         </a>
       ))}
