1cfd0da7a805df39e74a4bdc597ce63902d1f1be
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-22 14:33:37 -0600

    Whichever nav item is currently active, change background color to black

---
+++ frontend/src/components/NavDots.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavDots = ( { active }) => {
+  return (
+    <div className="app__navigation">
+      {['home', 'about', 'skills', 'projects', 'contact'].map((item, index) => (
+        <a href={`#${item}`} 
+          className="app__navigation-dot"
+          key={item + index}
+          style={active === item ? {backgroundColor: '#000'} : '' }
+        >
+          {item}
+        </a>
+      ))}
+    </div>
+  )
+}
+
+export default NavDots
