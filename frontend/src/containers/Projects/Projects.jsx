429e101e29b886cc17c7a20ad226251342479b6a
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-22 16:07:10 -0600

    Initial setup for projects

--- frontend/src/containers/Projects/Projects.jsx
+++ frontend/src/containers/Projects/Projects.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Projects.scss';
+import { motion } from 'framer-motion';
+import { BsFillEyeFill, BsGithub } from 'react-icons/bs';
+import { AppWrapper } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 const Projects = () => {
+
   return (
-    <div>Projects</div>
+    <>
+    <h2 className="head-text">
+      My <span>Projects</span> 
+    </h2>
+
+    </>
   )
 }
 
