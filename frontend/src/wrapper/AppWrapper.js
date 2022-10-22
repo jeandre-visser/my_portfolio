278a3346bd1c31077324f8b4ed6457549c837ab6
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-22 14:42:10 -0600

    Provide the classNames dynamically, render specific component, and add NavDots to each component

--- frontend/src/wrapper/AppWrapper.js
+++ frontend/src/wrapper/AppWrapper.js
@@ -3,7 +3,12 @@ import { NavDots } from '..components/NavDots';
 
 const AppWrapper = (Component, classNames, idName) => function HOC() {
   return (
-    <div>AppWrapper</div>
+    <div className={`app__container ${classNames}`} id={idName}>
+      <div className="app__wrapper app__flex">
+        <Component />
+      </div>
+      <NavDots active={idName} />
+    </div>
   )
 }
 
