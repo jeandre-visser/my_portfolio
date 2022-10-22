29a2c17515a835f9e737cb5cd3c57faac6b54ff6
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-22 14:32:09 -0600

    Initial setup for wrapper

---
+++ frontend/src/wrapper/AppWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavDots } from '..components/NavDots';
+
+const AppWrapper = () => {
+  return (
+    <div>AppWrapper</div>
+  )
+}
+
+export default AppWrapper
