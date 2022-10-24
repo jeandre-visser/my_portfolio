9fc3fbcd32415fd5e7036242828e2385f5571787
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-24 17:16:42 -0600

    Create employment section

--- frontend/src/containers/Employment/Employment.jsx
+++ frontend/src/containers/Employment/Employment.jsx
@@ -1,19 +1,36 @@
 import './Employment.scss';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { AppWrapper } from '../../wrapper';
-import { urlFor, client } from '../../client';
+
+const jobs = [
+  { title: 'Junior Project Manager', company: 'Terralogix Solutions Inc.', industry: 'Environmental Services', year: '2020 - 2021', location: 'Edmonton, Alberta, Canada', imgUrl: {}},
+  { title: 'Junior Operator - Summer Terms', company: 'Cenovus Energy', industry: 'Oil and Gas', year: '2017 - 2019', location: 'Alberta, Canada', imgUrl: {}},
+]
 
 const Employment = () => {
+
   return (
     <>
       <h2 className="head-text"><span>Employment  </span>History</h2>
-      <div className="app__employment-container">
-        <motion.div 
-          className="app__experience-list"
-        >
-
-        </motion.div>
+      <div className="app__employment">
+        {jobs.map((job, index) => (
+          <motion.div
+            className="app__employment-item"
+            whileInView={{ x: [300, 0], opacity: [0, 1] }}
+            transitions={{ duration: 2, type: 'tween' }}
+            key={job.title + index}
+          >
+            <p className="p-text">{job.year}</p>
+            <img src={job.imgUrl} alt={job.title} />
+            <div className="app__employment-desc">
+              <h2 className="bold-text">{job.title}</h2>
+              <p className="p-text">{job.company}</p>
+              <p className="p-text">{job.industry}</p>
+              <p className="p-text">{job.location}</p>
+            </div>
+          </motion.div>
+        ))}
       </div>
     </>
   )
