471f1383edea7c3689888375ddfd232d1d453359
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-24 15:54:37 -0600

    Add Education and Employment links

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo"/>
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'skills', 'projects', 'contact'].map((item) => (
+        {['home', 'about', 'skills', 'projects', 'education', 'employment', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <a href={`#${item}`}>{item}</a>
           </li>
@@ -30,7 +30,7 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'skills', 'projects', 'contact'].map((item) => (
+              {['home', 'about', 'skills', 'projects', 'education', 'employment', 'contact'].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
