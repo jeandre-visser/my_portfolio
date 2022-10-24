25c7fef0fd7fc924c655e1bd83c8fc4e477a69d1
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-24 14:42:11 -0600

    Imports

--- frontend/src/containers/Skills/Skills.jsx
+++ frontend/src/containers/Skills/Skills.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
 import './Skills.scss';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { BsGithub } from 'react-icons/bs';
+import { BiLink } from 'react-icons/bi';
+import { AppWrapper } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 const Skills = () => {
   return (
