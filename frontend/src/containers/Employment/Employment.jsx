f977171d2df0dd5a4a2d89dba99b8b8b9ff78710
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-24 16:11:56 -0600

    Imports and initial code

--- frontend/src/containers/Employment/Employment.jsx
+++ frontend/src/containers/Employment/Employment.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import './Employment.scss';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { AppWrapper } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 const Employment = () => {
   return (
-    <div>Employment</div>
+    <>
+      <h2 className="head-text"><span>Employment  </span>History</h2>
+      <div className="app__employment-container">
+        <motion.div 
+          className="app__experience-list"
+        >
+
+        </motion.div>
+      </div>
+    </>
   )
 }
 
