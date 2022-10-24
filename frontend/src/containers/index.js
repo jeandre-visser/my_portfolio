3f61a848ac36699f356ce08532f7680997182ff3
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-24 15:48:10 -0600

    Export education and experience containers from index,js

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -3,4 +3,6 @@ export { default as Footer } from './Footer/Footer';
 export { default as Header } from './Header/Header';
 export { default as Projects } from './Projects/Projects';
 export { default as Skills } from './Skills/Skills';
+export { default as Education } from './Education/Education';
+export { default as Experience } from './Experience/Experience';
 export { default as Testimonial } from './Testimonial/Testimonial';
