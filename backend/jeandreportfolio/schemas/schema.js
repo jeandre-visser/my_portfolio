e7252b7760a93371fcd61f77edd411ce6fb11596
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-25 16:00:34 -0600

    Clean up schemas, remove unneccassary files

--- backend/jeandreportfolio/schemas/schema.js
+++ backend/jeandreportfolio/schemas/schema.js
@@ -4,12 +4,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import projects from './projects'
-import testimonials from './testimonials'
-import brands from './brands'
 import abouts from './abouts'
-import experiences from './experiences'
 import skills from './skills'
-import workExperience from './workExperience'
 import contact from './contact'
 
 
@@ -18,6 +14,6 @@ export default createSchema({
 
   name: 'default',
 
-  types: schemaTypes.concat([projects, testimonials, brands, abouts, skills, workExperience, experiences, contact
+  types: schemaTypes.concat([projects, abouts, skills, contact
   ]),
 })
