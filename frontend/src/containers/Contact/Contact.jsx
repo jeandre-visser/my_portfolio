b0df38d2d0012a97db0676386d7c1f7c8ecbb4ac
Jeandre Visser <jeandrev1414@gmail.com>
2022-10-26 15:41:05 -0600

    Add a footer

--- frontend/src/containers/Contact/Contact.jsx
+++ frontend/src/containers/Contact/Contact.jsx
@@ -6,7 +6,7 @@ import './Contact.scss';
 const contacts = [
   {title: 'LinkedIn', url: 'http://www.linkedin.com/in/jeandre-visser', img: images.linkedin},
   {title: "GitHub", url: 'https://www.github.com/jeandre-visser', img: images.github},
-  {title: 'jeandrev1414@gmail.com', url: '', img: ''},
+  {title: 'jeandrev1414@gmail.com', url: '', img: images.email},
   {title: 'Resume', url: 'https://resume.creddle.io/resume/9fih5w59z8y', img: images.resume},
   {title: '639-840-1087', url: '', img: images.phone},
 ]
@@ -15,7 +15,7 @@ const Contact = () => {
   return (
     <>
       <h2 className="head-text" id="heading">Get in <span style={{ color: 'var(--orange-color'}}>touch</span></h2>
-      <div className="app__flex app__contacts">
+      <div className="app__contacts">
         {contacts.map((contact, index) => (
           <motion.div 
           className="app__contact-card" 
@@ -26,11 +26,19 @@ const Contact = () => {
             <a href={contact.url}>
               <img src={contact.img} alt="contact-img"></img>
             </a>
-            <h2>{contact.title}</h2>
+            <h3>{contact.title}</h3>
           </motion.div>
         ))}
-
       </div>
+      <motion.div
+        className="app__footer"
+        transition={{ duration: 1, ease: 'easeInOut'}}
+        whileInView={{ opacity: [0, 1] }}
+      >
+        <img src={images.footer} alt='logo'/>
+        <p className="p-text">Jeandre Visser<br/>2022</p>
+      </motion.div>
+
     </>
   )
 }
