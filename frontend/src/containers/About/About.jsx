55ab265be7f9198566952c62f73352fcd3d7855a
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-04 11:03:49 -0600

    Add hobby cards

--- frontend/src/containers/About/About.jsx
+++ frontend/src/containers/About/About.jsx
@@ -6,6 +6,12 @@ import { urlFor, client } from '../../client';
 import { AppWrapper } from '../../wrapper';
 import { GoLocation } from 'react-icons/go';
 
+
+const hobbies = [
+  {title: 'hitting the gym', imgUrl: images.gym}, {title: 'enjoying outdoors', imgUrl: images.outdoors}, {title: 'drinking coffee', imgUrl: images.coffee},
+];
+
+
 const About = () => {
 
   const [abouts, setAbouts] = useState([]);
@@ -47,6 +53,7 @@ const About = () => {
 
       <p className='p-text'>My name is <strong>Jeandre Visser</strong>, in search of a career in full-stack or front-end development. I am eager to continue learning and collaborate with other developers of various experience levels. As a graduate from <a href="https://www.lighthouselabs.ca/">Lighthouse Labs</a>, I was involved in a fast-paced learning environment comprised of small to large projects using popular front and back-end languages. I also transitioned from the oil and gas industry as a former project manager at an environmental engineering consulting company with transferable skills in problem-solving and project management that will help to kickstart a new career in programming!</p>
     </motion.div>
+    
     <motion.div
           whileInView={{ opacity: [0, 1] }}
           transitions={{ duration: 2 }}
@@ -54,7 +61,7 @@ const About = () => {
         >
           <div>
             <img src={images.canada} alt="canada" />
-            <h4>English (Canada)</h4>
+            <h4>English</h4>
           </div>
 
           <div>
@@ -63,6 +70,24 @@ const About = () => {
           </div>
           
         </motion.div>
+
+    <h1 className='app__hobby-title' style={{ margin: '4rem 0 1rem', textAlign: 'center'}}>When I am not <span style={{color: 'var(--secondary-color)'}}>coding</span>, I am ...</h1>
+    <div className="app__hobbies app__flex">
+      
+      {hobbies.map((hobby, index) => (
+        <motion.div
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transitions={{ duration: 2, type: 'tween' }}
+          key={hobby.title + index}
+          whileHover={{ scale: 1.05 }}
+          className="app__profile-item"
+          style={{ margin: '1rem 3rem'}}
+        >
+          <img src={hobby.imgUrl} alt={hobby.title} />
+          <h2 className="bold-text" style={{ marginTop: 8, textTransform: 'capitalize'}}>{hobby.title}</h2>
+        </motion.div>
+      ))}
+    </div>
     </>
   )
 }
