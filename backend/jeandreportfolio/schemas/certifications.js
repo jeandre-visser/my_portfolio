c0b4c8dc1fbe2bbad026ff18aa64fffb85926e37
Jeandre Visser <jeandrev1414@gmail.com>
2023-04-16 15:01:00 -0600

    Add certifications schema

---
+++ backend/jeandreportfolio/schemas/certifications.js
@@ -0,0 +1,32 @@
+export default {
+  name: "projects",
+  title: "Projects",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+
+    {
+      name: "shadow",
+      title: "Box Shadow Color",
+      type: "string",
+    },
+
+    {
+      name: "courseLink",
+      title: "Project Link",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImageUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
